1019be40be428aca8c13adc4e50898b7610045a0
Sven <thijssen_sven@hotmail.com>
2019-03-07 17:15:01 +0100

    minor correction

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ def system(vect, t):
 
 def main():
     environment = PrisonersDilemmaEnvironment()
-    nr_episodes = 10000
-
-    player_a = QLearning(environment)
-    player_b = QLearning(environment)
-    for ep in range(nr_episodes):
-        environment.action_player_one(player_a.action())
-        environment.action_player_two(player_b.action())
-
-        player_a.reward(environment.PlayerOneAction, environment.reward_player_one())
-        player_b.reward(environment.PlayerTwoAction, environment.reward_player_two())
-
-        print(player_a.q_table)
-        print(player_b.q_table)
+    # nr_episodes = 10000
+    #
+    # player_a = QLearning(environment)
+    # player_b = QLearning(environment)
+    # for ep in range(nr_episodes):
+    #     environment.action_player_one(player_a.action())
+    #     environment.action_player_two(player_b.action())
+    #
+    #     player_a.reward(environment.PlayerOneAction, environment.reward_player_one())
+    #     player_b.reward(environment.PlayerTwoAction, environment.reward_player_two())
+    #
+    #     print(player_a.q_table)
+    #     print(player_b.q_table)
 
 
     # DYNAMICS
@@ -40,11 +40,11 @@ def main():
 
     # arange(start, stop, step)
     xs_mesh, ys_mesh = meshgrid(arange(0, 1, .2), arange(0, 1, .2))
-    U = dynamics.get_mesh_dynamics(xs_mesh, ys_mesh, 0)
-    V = dynamics.get_mesh_dynamics(xs_mesh, ys_mesh, 1)
+    us = dynamics.get_mesh_dynamics(xs_mesh, ys_mesh, 0)
+    vs = dynamics.get_mesh_dynamics(ys_mesh, xs_mesh, 1)
 
-    figure()
-    quiver(U, V)
+    fig, ax = plt.subplots()
+    ax.quiver(xs_mesh, ys_mesh, us, vs)
     show()
 
 
